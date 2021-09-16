ecb84ff2fbc4ba79821170b6aa256d65a54944c2
Adam Lillo <alillo2@student.cvtc.edu>
2021-09-16 16:25:18 -0500

    [finishes

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -3,25 +3,46 @@ let green = document.getElementById('green');
 let blue = document.getElementById('blue');
 let box = document.querySelector('div.square_canvas');
   
-let r = 0, g = 0, b = 0;
-  
+let r = 0 , g = 0, b = 0;
+var rho = 0.0;
+var gamma;
+var beta;
+var luminance;
+
 red.addEventListener("keyup", function (event) {
-    r = red.value;
+    r = red.value * 255;
     if (!r)
         r = 0;
     box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    
+    
 });
   
 green.addEventListener("keyup", function (event) {
-    g = green.value;
+    g = green.value * 255;
     if (!g)
         g = 0;
     box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 });
   
 blue.addEventListener("keyup", function (event) {
-    b = blue.value;
+    b = blue.value * 255;
     if (!b)
         b = 0;
     box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    
 }); 
+
+let btn = document.createElement("button");
+    btn.innerHTML = "Convert";
+    btn.onclick = function () {
+    rho = 1 / (r + g + b) * r;
+    gamma = 1 / (r + g + b) * g;
+    beta = 1 / (r + g + b) * b;
+    luminance = Math.max(r,g,b);
+    luminance = luminance / 255;
+        alert(" rho = " + rho.toFixed(2) + " gamma = " + gamma.toFixed(2) + " beta = " + beta.toFixed(2) + " luminance = " + luminance);
+};
+document.body.appendChild(btn);
+
+
